622bd5bca7c37bd910b88ac97d6deb9424ed0062
zollder <goodzollder@gmail.com>
2016-10-19 02:44:33 -0400

    Configured Express-based http server. Added socket.io dependency.

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
+// load Express and http modules
+var express = require('express');
 var http = require('http');
-var serveStatic = require('serve-static');
 
-/*
- * https://github.com/expressjs/serve-static#serve-files-with-vanilla-nodejs-http-server Serve up public/ folder
- */
-var servePublic = serveStatic('public', {
-	'index' : [ 'index.html', 'index.htm' ]
+// build the app
+var app = express();
+
+// logging middleware
+app.use(function(request, response, next) {
+	console.log(request.method, request.url, 'HTTP' + request.httpVersion);
+	next();
 });
 
-http.createServer(function handler(req, res) {
-	console.log(req.method, req.url, 'HTTP' + req.httpVersion, req.headers); // , req is too long
+// response header configuration middleware
+app.use(function(request, response, next) {
+	response.setHeader("Content-Type", "text/plain" );
+	response.setHeader('Access-Control-Allow-Origin', '127.0.0.1');
+	next();
+});
 
-	res.setHeader('Access-Control-Allow-Origin', '127.0.0.1');
-	servePublic(req, res, function nextHandler(req, res) {});
-}).listen(8086, '127.0.0.1');
+// error-handling middleware
+app.use(function(error, request, response, next) {
+	if (error) {
+		console.error(error.stack);
+		response.status(500).send("An error occured!");
+		next(error);
+	}
+	next();
+});
 
-console.log('Server running at http://127.0.0.1:1337/');
+// add static resource handler as a middleware to Express
+app.use(express.static('public', {'index' : [ 'index.html', 'index.htm' ]}));
+
+// bind and listen for connections on specified host and port
+http.createServer(app).listen(8086, '127.0.0.1', function() {
+	console.log('Server running at http://127.0.0.1:8086/');
+});
